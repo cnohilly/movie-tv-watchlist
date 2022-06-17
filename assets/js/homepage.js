0d68875d9a5eb228cb7a67bc9a76051676ae69bb
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-06-17 15:55:12 -0400

    created function to call for api and for results to display

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,30 +1,100 @@
 // API Key
-var apiKey = "8da71b56dc2b287e98f9875203b81d2c";
+const apiKey = "8da71b56dc2b287e98f9875203b81d2c"
+const imgURL = "https://image.tmdb.org/t/p/w500/"
+// API Endpoint
+const url = "https://api.themoviedb.org/3/search/movie?api_key=8da71b56dc2b287e98f9875203b81d2c&query="
 
-var url = "https://api.themoviedb.org/3/search/movie?api_key=8da71b56dc2b287e98f9875203b81d2c&query=toy%20story"
-
-// Selecting elements from the DOM
-const buttonEl = document.querySelector("#search");
-const inputEl = document.querySelector("#inputValue");
-
+//selecting elements from the DOM
+const searchBtnEl = document.querySelector("#search")
+const inputEl = document.querySelector("#inputValue")
+const movieSearchableEl = document.querySelector("#movie-searchable")
 
 
 
+/* Writing some Javascript that will DYNAMICALLY give us OUTPUT BELOW
+      <div class="movie">
+       
+        <section class="section">
+          <img src="https://image/tmbd.org/t/p/w500/rZd0y1X1Gw4t5B3f01Qzj8DYY66.jpg"
+           alt="" 
+           data-movie-id="557" 
+           />
+          <img src="https://image/tmbd.org/t/p/w500/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg" 
+          alt="" 
+          data-movie-id="429617" 
+          />
+        </section>
+        
+        <div class="content">
+          <p id="content-close">X</p>
+        </div>
+      </div>
+*/     
 
-buttonEl.onclick = function(event) {
-    event.preventDefault();
-    const value = inputEl.value
+var movieSection = function(movies) {
+  return movies.map((movie) => {
+    if (movie.poster_path){
+      return `<img 
+        src=${imgURL + movie.poster_path} 
+        data-movie-id=${movie.id}/>`;
+     }
+  })
+}
+
+var createMovieContainer = function (movies){
+    // Created <div> in movie 
+    const movieEl = document.createElement("div");
+    movieEl.setAttribute("class", "movie");
+
+
+    const movieTemplate = `
+        <section class="section">
+         ${movieSection(movies)}
+        </section>
+        
+        <div class="content">
+          <p id="content-close">X</p>
+        </div>
+    `;
+
+    movieEl.innerHTML =  movieTemplate;
+    return movieEl;
+
+}
+
+const renderSearchMovies = function(data) {
+  // When user search another movie, the previous results will erase and display the new search results (movieSearchableEl.innerHTML= "";)
+     movieSearchableEl.innerHTML = "";
+     const movies = data.results
+     const movieBlock = createMovieContainer(movies);
+     movieSearchableEl.appendChild(movieBlock);
+     console.log("Data", data);
+}
+
+
+searchBtnEl.onclick = function(event) {
+event.preventDefault();
     
+// to save input value
+const value = inputEl.value;
 
-  fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-          console.log("Data: ", data);
-      })
-      .catch((error) => {
-        console.log("Error: ", error);
-      })
-  console.log("Value:", value);
+// Clear input bar
+inputEl.value = "";
+
+// Whatever is placed in inputEl, will be searched 
+const newUrl = url + "&query=" + value
+
+fetch(newUrl)
+.then((res) => res.json()) 
+.then(renderSearchMovies)
+.catch((error) => {
+    console.log("Error: ", error)
+});
+    
+console.log("Value ", value);
+
     
 }
+
+
 
